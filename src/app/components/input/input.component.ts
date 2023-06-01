21e1d048af18db4df8185fc9be724c012c0e5263
Jonas sousa <jonassousagtr@gmail.com>
2023-06-01 06:33:09 -0300

    Feat: Criando a funcionalidade de editar o item da lista.

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,17 @@
 import { ListaDeCompraService } from './../../service/lista-de-compra.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges{
+  @Input() itemQueSeraEditado!: Item
 
+  editando: boolean = false;
+  textoBtn: string = 'Salvar Item'
   valorItem!: string;
 
   constructor(
@@ -15,6 +19,22 @@ export class InputComponent implements OnInit {
   ) { }
 
   ngOnInit(): void { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(!changes['itemQueSeraEditado'].firstChange){
+      this.editando = true;
+      this.textoBtn = 'Editar Item'
+      this.valorItem = this.itemQueSeraEditado?.nome
+    }
+  }
+
+  editarItem(){
+    this.listaDeCompraService.editarItemDalista(
+      this.itemQueSeraEditado, this.valorItem)
+      this.limparCampo()
+      this.editando = false
+      this.textoBtn = 'Salvar Item'
+  }
 
   adicionarItem(){
     this.listaDeCompraService.adicionarItemNaLista(this.valorItem)
